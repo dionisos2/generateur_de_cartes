e997d81a61f03208513a145738902e11a2a4e01d
plumpaiok <plumpaiok@yahoo.fr>
2016-07-12 12:02:25 +0200

    Script adapté
    Le script en vesrion javascript, qui renvoie un tableau de tableaux.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ console.log(csvText);
 
 
 
-function parse_csv(csvText) {
+function parse_csv(leCSV) {
 
   // CONSTANTS
   var separationChar = ",";
@@ -24,36 +24,42 @@ function parse_csv(csvText) {
   var entry = [];
   var filteredEntries = [];
   var textField = "";
+  var depth = 0 ;
 
   // CONTROL OF ALL CHARS OF THE DATABASE TEXT TO SEPARATE THE FIELDS (;)
-  console.log("[order_csv] Verifying chars to separate the fields into " + csvText);
-  for (var i = 0; i < csvText.length; i++) {
+  console.log("[order_csv] Verifying chars to separate the fields into " + leCSV);
+  for (var i = 0; i < leCSV.length; i++) {
 
-    var char = csvText.charAt(i);
+    var lettre = leCSV.charAt(i);
     
 
     // Separation
-    if (char == separationChar) {
+    if (lettre == separationChar && depth==0) {
       entry.push(textField);
       textField = "";
-      console.log(i+' '+char+' séparation');
+      console.log(i+' '+lettre+' séparation');
     } else {
       // End of the line
-      if (char == endOfLineChar) {
+      if (lettre == endOfLineChar) {
         entry.push(textField);
         entries.push(entry);
         textField = "";
         entry = [];
-        console.log(i+' '+char+' fin de ligne');
+        console.log(i+' '+lettre+' fin de ligne');
       } else {
         //Quotation marks
-        if (char == quotationMarks) {
+        if (lettre == quotationMarks) {
+		  if (depth == 0){
+		  depth++;
+		 }else{
+		  depth--;
+		 }
           textField += "'";
-          console.log(i+' '+char+' guillemets');
+          console.log(i+' '+lettre+' guillemets');
         } else {
-          // Normal char
-          textField += char;
-          console.log(i+' '+char);
+          // Normal lettre
+          textField += lettre;
+          console.log(i+' '+lettre);
         }
       }
     }
@@ -62,7 +68,7 @@ function parse_csv(csvText) {
 
 /*
     // DELETION OF THE HEADERS AND TITLES (FIRST COLUMN IS NAN)
-    print "[order_csv] Removing the headers and titles into ordered " + csvText
+    print "[parse_csv] Removing the headers and titles into ordered " + leCSV
     for entry in entries:
 
       // Good entry : generate
@@ -78,7 +84,8 @@ function parse_csv(csvText) {
     // RETURNS THE ORGANIZED AND FILTERED ENTRIES
     return filteredEntries
       */
-  return textField ;
+	console.log(entries);
+  return entries ;
   }
   
 var untext = parse_csv(csvText) ;
