c69288311c15618d6dd23c6f90695d567cccabe2
plumpaiok <plumpaiok@yahoo.fr>
2016-07-12 14:06:30 +0200

    formulaire url et csv
    Ajout d'une iframe pour intégrer le Framacalc, et deux boutons pour
    déclencher le script

---
+++ formulaire.js
@@ -0,0 +1,39 @@
+var textForm ;
+var calcUrl ;
+var leCSV ;
+
+function updateCalc(laUrl){
+document.getElementById('calcPage').src = laURL;
+}
+
+function getCalcUrl(){
+    var getText = document.getElementById('textForm').value;
+	console.log("Voilà la récupération : ") ;
+	console.log(getText) ;
+	calcUrl = getText ;
+	//updateCalc(calcUrl) ;
+	document.getElementById('calcPage').src = getText;
+	return getText ;
+}
+
+function launchCSV(){
+leCSV = calcUrl+'.csv';
+console.log(leCSV);
+}
+
+function setForm(){
+textForm = document.getElementById('textForm');
+var urlButton = document.getElementById('URLbutton');
+urlButton.addEventListener('click', getCalcUrl);
+}
+
+function setCSV(){
+var csvButton = document.getElementById('CSVbutton');
+csvButton.addEventListener('click', launchCSV);
+}
+
+function main(){
+setCSV() ;
+setForm() ;
+}
+
