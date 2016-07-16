57f2cf46352eb6d1903eb1547875789452dcaecf
plumpaiok <plumpaiok@yahoo.fr>
2016-07-16 17:30:59 +0200

    pas grand chose
    supprimé des console log de partout

--- interface.js
+++ interface.js
@@ -1,37 +1,33 @@
+
+// EXTENSION DES IFRAMES AU SURVOL
+
 var frameCalc;
 var frameSvg;
 
 function activer(cible){
-console.log('activation!');
 cible.width = '90%' ;
 cible.height = '700px' ;
 }
 
 function desactiver(cible){
-console.log('desactivation!');
 cible.width = '5%' ;
 cible.height = '250px' ;
 }
 
 function activerCalc(){
-'activation calc!'
 activer(frameCalc);
 desactiver(frameSvg);
 }
 
 function activerSvg(){
-'activation svg!'
 activer(frameSvg);
 desactiver(frameCalc);
 }
 
 function setFrames(){
-console.log('ON SET FRAMES');
 
 frameCalc = document.getElementById('calcPage');
 frameSvg = document.getElementById('svgPage');
-console.log(frameCalc);
-//pagecalc.addEventListener('mouseover', activer);
 
 frameCalc.addEventListener("mouseover", activerCalc);
 frameSvg.addEventListener("mouseover", activerSvg);
